099fe007c6356c75557a7912bde000ae74bbd602
Congee <congeec@qq.com>
2023-07-15 14:18:56 -0400

    chore(pkgs): remove devspace, mockoon, cider, and pam-reattach
    They are already in nixpkgs

--- default.nix
+++ default.nix
@@ -14,7 +14,6 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  devspace = pkgs.callPackage ./pkgs/devspace { };
   kim = pkgs.callPackage ./pkgs/kim { };
   pinentry-touchid = pkgs.callPackage ./pkgs/pinentry-touchid { };
   pam-touchid = pkgs.callPackage ./pkgs/pam-touchid { };
@@ -23,6 +22,4 @@
   whereami = pkgs.callPackage ./pkgs/whereami { };
   openssl-tpm2-engine = pkgs.callPackage ./pkgs/openssl-tpm2-engine { };
   ibmtss = pkgs.callPackage ./pkgs/ibmtss { };
-  cider = pkgs.callPackage ./pkgs/cider { };
-  mockoon = pkgs.callPackage ./pkgs/mockoon { };
 }
